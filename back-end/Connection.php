46a9d2380df0fdca6304b66af751d11364a0d3ea
Júlio de Oliviera <ramosju423@gmail.com>
2024-04-27 23:06:07 -0300

    feat: User can sign in and log in

--- back-end/Connection.php
+++ back-end/Connection.php
@@ -1,10 +1,12 @@
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 $mysqli = new mysqli("localhost", "", "", "customers");
 
 if ($mysqli -> connect_errno) {
     echo "Failed to connect: " . $mysqli -> connection_error;
     exit();
 }
-
-$zika = 0;
