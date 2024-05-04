a3fc78b383c199a7081f69939568bbaf70637fd8
Júlio de Oliviera <ramosju423@gmail.com>
2024-05-04 20:20:51 -0300

    Update classes.php

--- back-end/classes.php
+++ back-end/classes.php
@@ -84,7 +84,6 @@ class playlistActions
         $newJson = json_encode($arrayPlaylist);
         $updatePlaylist = "UPDATE users SET playlist = '$newJson' WHERE username = '$userName'";
         $update = mysqli_query($mysqli, $updatePlaylist);
-        dump($arrayPlaylist);
     }
 
     public static function addMusicToPlaylist($userName, $idPlaylist, $idMusic, $band, $musicTitle)
@@ -124,9 +123,7 @@ class playlistActions
     public static function removeMusicFromPlaylist($userName, $idPlaylist, $idMusic)
     {
 
-        //TODO: adicionar funcionalidade, (no return), adicionar parâmetros: iddaplaylist e idmusica
         require "./Connection.php";
-        // Ideia: pede id no parametro...
         $query = "SELECT playlist FROM users WHERE username = '$userName'";
         $result = mysqli_query($mysqli, $query);
 
@@ -136,7 +133,6 @@ class playlistActions
 
         
         if ($arrayPlaylist[$idPlaylist]['musics'][$idPlaylist]['id'] == $idMusic) {
-            dump("Música encontrada!");
             unset($arrayPlaylist[$idPlaylist]['musics'][$idPlaylist]);
             
         } else {
@@ -147,22 +143,28 @@ class playlistActions
         $newJson = json_encode($arrayPlaylist);
         $update = "UPDATE users set playlist = '$newJson' WHERE username = '$userName'";
         $runUpdate = mysqli_query($mysqli, $update);
-        
-        // Bota em objeto php, modifica, e dps volta pra JSON (string) e update na tabela;
-        // var_dump(json_decode($playlist["playlist"]));
     }
 
-    public static function getFullPlaylistData()
+    public static function getFullPlaylistData($userName)
     {
         //TODO: precisa retornar info da playlist, adicionar parâmetros: iddaplaylist
         require "./Connection.php";
 
-        $query = "SELECT playlist FROM users";
+        // Verifica se existe usuário
+        $getUser = "SELECT username FROM users WHERE username = '$userName'";
+        $result = mysqli_query($mysqli, $getUser);
+        $result = mysqli_fetch_assoc($result);
+
+        if ($result == null) {
+            exit("Usuário não encontrado!");
+        }
+
+        $query = "SELECT playlist FROM users WHERE username = '$userName'";
         $result = mysqli_query($mysqli, $query);
 
         $playlist = mysqli_fetch_assoc($result);
 
-        var_dump(json_decode($playlist["playlist"]));
+        return $playlist;
     }
 }
 
@@ -181,6 +183,7 @@ class userActions
             $query = "INSERT INTO users VALUES (0, '$username', '$password', '{}')";
             $updatingTable = mysqli_query($mysqli, $query);
             return "Usuário cadastrado!";
+
         } else {
             http_response_code(400);
             return "Nome de usuário já existe, tente outro nome!";
@@ -202,6 +205,4 @@ class userActions
     }
 }
 
-playlistActions::removeMusicFromPlaylist('Coroa', 1, "12");
-// $resulting->addMusicToPlaylist("Farelo", 0, 3, "Linkin Park", "In the end");
-//TODO: create new method to create playlist createPlaylist();
+playlistActions::getFullPlaylistData('Júlio');
