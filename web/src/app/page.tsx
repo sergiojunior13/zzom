5c3cb80979177554d754345be710a81abeccf69b
Cauã <143448247+CauaMarques12@users.noreply.github.com>
2024-05-06 20:13:12 -0300

    feat: add feedback area

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Home, Search, Library } from "lucide-react";
+import { Home, Search, Library, ClipboardPenLine } from "lucide-react";
 import { AsideMenu } from "@/components/asidemenu";
 import { PlayerFooter } from "@/components/playerfooter";
 import { useState, useEffect } from "react";
@@ -20,8 +20,13 @@ export default function HomeLayout() {
       <hr className="mr-8 ml-8 border-zinc-700" />
       <div className="p-8 mb-8 flex justify-between">
         <p className="ml-6">&copy; ZZOM</p>
-        <a href="" className="mr-6 hover:underline">
-          Deixe seu feedback
+        <a
+          title="Deixe sua avaliação"
+          href="https://forms.gle/BTXwaj4BF7VmKHpY6"
+          target="_blank"
+          className="mr-6 hover:underline"
+        >
+          <ClipboardPenLine />
         </a>
       </div>
     </main>
